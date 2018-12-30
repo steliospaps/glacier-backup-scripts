a296aefab82c0ecb97de420678f5fa8ca1bdb68b
stelios <stelios05@gmail.com>
2018-12-30 16:02:34 +0000

    initial scripts before first use in EC2

---
+++ aws_glacier_file.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -x
+set -eo pipefail
+if [[ $# -lt 3 ]]
+then
+	echo 'usage :
+'$0' <vault-name> <contents-filename> <pattern>...' >&2
+	exit 1 
+fi
+export VAULT=${1:?}
+export CONTENTS_FILENAME="${2:?}.txt"
+export CONTENTS_FILE="$HOME/Private/aws/glacier/$CONTENTS_FILENAME"
+shift 2
+
+for f in "$@" 
+do
+	FILENAME=${f##*/}
+	echo $FILENAME >> $CONTENTS_FILE || exit 1
+	aws glacier upload-archive --account-id - --vault-name $VAULT --archive-description $FILENAME --body $f  >> $CONTENTS_FILE || exit $?
+done
+
+aws s3 cp "$CONTENTS_FILE" "s3://stelios-bucket/vaults/backup/$CONTENTS_FILENAME" 
---
+++ volume_change_script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# will expect an archive of the form xxxx.SERIAL.xxx
+# example:
+# tar cvf stelios.backups.helena.SERIAL.tar -M -L4G -F ~/bin/volume_change_script.sh -C ~/Dropbox stelios/backups/helena
+#enable extended patterns
+#shopt -s extglob
+
+echo TAR_ARCHIVE=$TAR_ARCHIVE
+echo TAR_FD=$TAR_FD
+echo TAR_VOLUME=$TAR_VOLUME
+NUMBER=$(printf "%03d" $TAR_VOLUME)
+echo NUMBER=$NUMBER
+NAME=${TAR_ARCHIVE/.SERIAL./.$NUMBER.}
+echo NAME=$NAME
+echo ${NAME} >&$TAR_FD
+bzip2 $TAR_ARCHIVE 
+
